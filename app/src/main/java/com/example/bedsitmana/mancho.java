ef23047da5d9a5fed8be4508cfdc87e4001ff4ca
HuyBunn <130323865+HuyBunn@users.noreply.github.com>
2023-11-10 22:59:37 +0700

    v2.3.6

--- app/src/main/java/com/example/bedsitmana/mancho.java
+++ app/src/main/java/com/example/bedsitmana/mancho.java
@@ -1,10 +1,13 @@
 package com.example.bedsitmana;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class mancho extends AppCompatActivity {
 
@@ -12,12 +15,15 @@ public class mancho extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mancho);
+        LottieAnimationView animationView = findViewById(R.id.anm);
+        animationView.setSpeed(0.5f);
+        animationView.playAnimation();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(mancho.this,dangnhap.class));
                 finish();
             }
-        },3500);
+        },5000);
     }
 }
