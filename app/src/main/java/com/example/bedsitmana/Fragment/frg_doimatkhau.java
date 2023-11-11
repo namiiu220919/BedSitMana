321772792398e580bcf8d0f9ed259963370eda52
TuanQuynh0308 <117746207+TuanQuynh0308@users.noreply.github.com>
2023-11-11 10:40:07 +0700

    v2.4.5

--- app/src/main/java/com/example/bedsitmana/Fragment/frg_doimatkhau.java
+++ app/src/main/java/com/example/bedsitmana/Fragment/frg_doimatkhau.java
@@ -1,4 +1,4 @@
-package com.example.bedsitmana;
+package com.example.bedsitmana.Fragment;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.bedsitmana.R;
 
 public class frg_doimatkhau extends Fragment {
 
