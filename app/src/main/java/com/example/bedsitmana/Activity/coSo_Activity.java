868dcd48d459dca89324b547563e5a86b1a34b16
HuyBunn <130323865+HuyBunn@users.noreply.github.com>
2023-11-12 15:24:30 +0700

    v2.5.6

--- app/src/main/java/com/example/bedsitmana/Activity/coSo_Activity.java
+++ app/src/main/java/com/example/bedsitmana/Activity/coSo_Activity.java
@@ -6,11 +6,24 @@ import androidx.appcompat.widget.Toolbar;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
+import com.example.bedsitmana.Adapter.Coso_Adapter;
+import com.example.bedsitmana.Adapter.NguoiThue_Adapter;
+import com.example.bedsitmana.Dao.coSoDao;
+import com.example.bedsitmana.Dao.nguoiThueDao;
 import com.example.bedsitmana.R;
+import com.example.bedsitmana.model.Coso;
+import com.example.bedsitmana.model.NguoiThue;
+
+import java.util.ArrayList;
 
 public class coSo_Activity extends AppCompatActivity {
-
+    ListView lstCoso;
+    ArrayList<Coso> list;
+    Coso_Adapter cosoAdapter;
+    Coso coso;
+    coSoDao dao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,5 +43,10 @@ public class coSo_Activity extends AppCompatActivity {
                 finish();
             }
         });
+        lstCoso = findViewById(R.id.lstCoSo);
+        dao = new coSoDao(coSo_Activity.this);
+        list = (ArrayList<Coso>) dao.getAll();
+        cosoAdapter = new Coso_Adapter(coSo_Activity.this,list,this);
+        lstCoso.setAdapter(cosoAdapter);
     }
 }
--- app/src/main/java/com/example/bedsitmana/Adapter/Coso_Adapter.java
+++ app/src/main/java/com/example/bedsitmana/Adapter/Coso_Adapter.java
@@ -1,4 +1,61 @@
 package com.example.bedsitmana.Adapter;
 
-public class Coso_Adapter  {
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.bedsitmana.Activity.coSo_Activity;
+import com.example.bedsitmana.R;
+import com.example.bedsitmana.model.Coso;
+
+import java.util.ArrayList;
+
+public class Coso_Adapter extends ArrayAdapter<Coso> {
+    TextView txtCoso,txtDiaChi,txtPhiDV,txtGiaDien,txtGiaNuoc;
+    Button btnXoa;
+private  Context context;
+private  ArrayList<Coso> list;
+coSo_Activity coSo_activity;
+
+
+    public Coso_Adapter(@NonNull Context context,ArrayList<Coso> list, coSo_Activity coSo_activity) {
+        super(context,0 ,list);
+        this.context = context;
+        this.list = list;
+        this.coSo_activity = coSo_activity;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View v = convertView;
+        if(v==null){
+            LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+            v=inflater.inflate(R.layout.item_coso,null);
+        }
+        final Coso coso = list.get(position);
+        if(coso!=null){
+            txtCoso = v.findViewById(R.id.txtCoSo);
+            txtDiaChi = v.findViewById(R.id.txtDiaChi);
+            txtPhiDV = v.findViewById(R.id.txtPhiDichVu);
+            txtGiaDien = v.findViewById(R.id.txtGiaDien);
+            txtGiaNuoc = v.findViewById(R.id.txtGiaNuoc);
+
+            txtCoso.setText("Cơ sở: "+coso.getTenCoso());
+            txtDiaChi.setText("Địa chỉ: "+coso.getDiaChi());
+            txtPhiDV.setText("Phí dịch vụ: "+coso.getPhiDichVu()+"/người");
+            txtGiaDien.setText("Giá điện: "+coso.getGiaDien()+"/số");
+            txtGiaNuoc.setText("Giá nước: "+coso.getGiaNuoc()+"/người");
+        }
+        return v;
+    }
 }
