b1967f00aa1c35e677803ee9810ca8b3622e11f1
HuyBunn <130323865+HuyBunn@users.noreply.github.com>
2023-11-23 21:59:16 +0700

    v2.9.6

--- app/src/main/java/com/example/bedsitmana/Fragment/frg_doimatkhau.java
+++ app/src/main/java/com/example/bedsitmana/Fragment/frg_doimatkhau.java
@@ -1,5 +1,8 @@
 package com.example.bedsitmana.Fragment;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +10,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.bedsitmana.Dao.nguoiThueDao;
 import com.example.bedsitmana.R;
+import com.example.bedsitmana.model.NguoiThue;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class frg_doimatkhau extends Fragment {
-
+    TextInputEditText txtMkc, txtMkm, txtNlMkm;
+    Button btnXN;
+    nguoiThueDao dao;
     public frg_doimatkhau() {
         // Required empty public constructor
     }
@@ -20,6 +30,56 @@ public class frg_doimatkhau extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frg_doimatkhau, container, false);
+        View v= inflater.inflate(R.layout.fragment_frg_doimatkhau, container, false);
+        txtMkc = v.findViewById(R.id.edtPassCu);
+        txtMkm = v.findViewById(R.id.edtPassMoi_dmk);
+        txtNlMkm = v.findViewById(R.id.edtNhapLaiPass_dmk);
+        btnXN = v.findViewById(R.id.btnXacNhan);
+        dao = new nguoiThueDao(getActivity());
+
+        btnXN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences preferences = getActivity().getSharedPreferences("USER_FILE", MODE_PRIVATE);
+                String user = preferences.getString("USERNAME", "");
+                if (validate() > 0) {
+                   NguoiThue nt = dao.getID(user);
+                    nt.setMatKhauNT(txtMkm.getText().toString());
+                    dao.update(nt);
+                    if (dao.update(nt)>0) {
+                        Toast.makeText(getActivity(), "Đổi mật khẩu thành công ", Toast.LENGTH_SHORT).show();
+                        txtMkc.setText("");
+                        txtMkm.setText("");
+                        txtNlMkm.setText("");
+
+                    } else {
+                        Toast.makeText(getActivity(), "Đổi mật khẩu thất bại ", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        return v;
+
+    }
+    public int validate() {
+        int check = 1;
+        if (txtMkc.getText().length() == 0 || txtMkm.getText().length() == 0 || txtNlMkm.getText().length() == 0) {
+            Toast.makeText(getActivity(), "Hãy nhập đầy đủ ", Toast.LENGTH_SHORT).show();
+            check = -1;
+        } else {
+            SharedPreferences preferences = getActivity().getSharedPreferences("USER_FILE", MODE_PRIVATE);
+            String passOld = preferences.getString("PASSWORD", "");
+            String passNew = txtMkm.getText().toString();
+            String RepassNew = txtNlMkm.getText().toString();
+            if (!passOld.equals(txtMkc.getText().toString())) {
+                Toast.makeText(getActivity(), "Mật khẩu cũ sai ", Toast.LENGTH_SHORT).show();
+                check = -1;
+            }
+            if (!passNew.equals(RepassNew)) {
+                Toast.makeText(getActivity(), "Mật khẩu mới không trùng ", Toast.LENGTH_SHORT).show();
+                check = -1;
+            }
+        }
+        return check;
     }
 }
--- app/src/main/java/com/example/bedsitmana/MainActivity.java
+++ app/src/main/java/com/example/bedsitmana/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.bedsitmana;
 
-import android.app.ActivityManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -22,7 +22,10 @@ import com.example.bedsitmana.Activity.nguoiThue_Activity;
 import com.example.bedsitmana.Activity.phong_Activity;
 import com.example.bedsitmana.Activity.suCo_Activity;
 import com.example.bedsitmana.Activity.thongKe_Activity;
+import com.example.bedsitmana.Dao.nguoiThueDao;
+import com.example.bedsitmana.Fragment.frg_doimatkhau;
 import com.example.bedsitmana.Fragment.frg_thongtintaikhoan;
+import com.example.bedsitmana.model.NguoiThue;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
@@ -37,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
     View mHeader;
     AdView mAdView;
     Button btnLoaiPhong, btnPhong, btnNguoiThue, btnThongKe, btnHoaDon, btnSuCo;
+    nguoiThueDao dao;
+    TextView txtUser;
 
 
     @Override
@@ -62,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+        mHeader = nav.getHeaderView(0);
+        txtUser = mHeader.findViewById(R.id.txtUser);
         btnLoaiPhong = findViewById(R.id.btnLoaiPhong);
         btnHoaDon = findViewById(R.id.btnHoaDon);
         btnPhong = findViewById(R.id.btnPhong);
@@ -81,7 +88,6 @@ public class MainActivity extends AppCompatActivity {
                 IntentClass(hoaDon_Activity.class);
             }
         });
-        
         btnPhong.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -109,27 +115,45 @@ public class MainActivity extends AppCompatActivity {
 
         nav.setItemIconTintList(null);
         drawerLayout.closeDrawers();
-
         SharedPreferences preferences = getSharedPreferences("user11", MODE_PRIVATE);
         String username = preferences.getString("username11", "...");
-
         Bundle bundle = new Bundle();
         bundle.putString("key", username);
         frg_thongtintaikhoan frgThongtintaikhoan=new frg_thongtintaikhoan();
         frgThongtintaikhoan.setArguments(bundle);
 
-
+        String user = username;
+        if(user.equalsIgnoreCase("admin")){
+            nav.getMenu().findItem(R.id.nav_ChangePass).setVisible(false);
+        }else {
+            btnLoaiPhong.setVisibility(View.GONE);
+            btnNguoiThue.setVisibility(View.GONE);
+            btnThongKe.setVisibility(View.GONE);
+            dao = new nguoiThueDao(this);
+            NguoiThue nt = dao.getID(user);
+            String u = nt.getTenNguoiThue();
+            txtUser.setText("Welcome "+u);
+        }
         nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if (item.getItemId()==R.id.nav_profileUser){
-
                     setTitle("Thông tin tài khoản");
+
                     replaceFrg(frgThongtintaikhoan);
                 }else if (item.getItemId()==R.id.nav_Home){
                     IntentClass(MainActivity.class);
+                } else if (item.getItemId()==R.id.nav_Loguot) {
+                    startActivity(new Intent(getApplicationContext(), dangnhap.class));
+                    finish();
+                }else if(item.getItemId()==R.id.nav_ChangePass){
+                    setTitle("Đổi mật khẩu");
+                    frg_doimatkhau frg_dmk=new frg_doimatkhau();
+                    replaceFrg(frg_dmk);
                 }
-                return false;
+
+                drawerLayout.close();
+                return true;
             }
         });
 
